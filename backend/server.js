f51f8773c039559d17b6960138d092320d5d4b9d
TommyTeaVee <fihlatv@gmail.com>
2024-11-17 20:31:52 +0200

    Added RAW files from different camera manufactures

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,33 @@ const cors = require('cors'); // Import cors
 
 const readFile = promisify(fs.readFile);
 
-
 const app = express();
-const upload = multer({ dest: 'uploads/' });
 // Enable CORS for all origins
 app.use(cors());
 
+// Multer configuration to accept additional raw image formats
+const upload = multer({
+    dest: "uploads/",
+    fileFilter: (req, file, cb) => {
+      const allowedTypes = [
+        "image/jpeg", // Standard JPEG
+        "image/png", // Standard PNG
+        "image/gif", // GIFs
+        "image/tiff", // TIFF files
+        "image/x-canon-cr2", // Canon RAW CR2
+        "image/x-canon-cr3", // Canon RAW CR3
+        "image/x-nikon-nef", // Nikon RAW NEF
+        "image/x-sony-arw", // Sony RAW ARW
+      ];
+  
+      if (allowedTypes.includes(file.mimetype)) {
+        cb(null, true);
+      } else {
+        cb(new Error("Only supported image types are allowed!"), false);
+      }
+    },
+  });
+  
 app.post('/extract', upload.single('file'), async (req, res) => {
   try {
     const filePath = req.file.path;
