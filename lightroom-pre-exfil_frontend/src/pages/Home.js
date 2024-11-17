dbd41c5a7ec916df4851549491d2d5f754d6a53e
TommyTeaVee <fihlatv@gmail.com>
2024-11-18 01:57:46 +0200

    added backdrop image instead of banner

--- lightroom-pre-exfil_frontend/src/pages/Home.js
+++ lightroom-pre-exfil_frontend/src/pages/Home.js
@@ -3,18 +3,22 @@ import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-br from-cyan-300 to-cyan-600 text-white">
-      <h1 className="text-5xl font-bold mb-4">
-        Welcome to  LightroomPreExfil, a  Preset Extractor made in Node.Js and React-App
-      </h1>
-      <p className="text-lg mb-8">
-        Upload your image files to extract their metadata or summary in just a few clicks.
-      </p>
-      <Link to="/upload">
-      <button className="bg-white text-blue-600 font-bold py-2 px-4 rounded hover:bg-gray-100 transition">
-          Get Started
-        </button>
-      </Link>
+    <div className="h-screen bg-cover bg-center bg-no-repeat relative" style={{ backgroundImage: `url('https://www.slrlounge.com/wp-content/uploads/2016/09/adobe-canon-5d-mkIV-lightroom-1600x1068.jpg')` }}>
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-cyan-700/60 mix-blend-multiply"></div>
+
+      {/* Content */}
+      <div className="relative flex flex-col items-center justify-center h-full text-white text-center px-4">
+        <h1 className="text-5xl font-bold mb-4">Welcome to LighroomxExfill</h1>
+        <p className="text-lg mb-8">
+          Upload your image files to extract their metadata or summary in just a few clicks.
+        </p>
+        <Link to="/upload">
+          <button className="bg-white text-cyan-700 font-bold py-2 px-4 rounded hover:bg-gray-100 transition">
+            Get Started
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
